80a562d2b2c357c04fe1c51bc90f31a7aa922b8f
Elias Renawi <elias.renawi@bizi.co.il>
2024-05-09 01:10:57 +0300

    undrstanding route

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* <main className="flex min-h-screen flex-col items-center justify-between p-24"> */}
       {/* <Headr /> */}
-      <Navbar />
+      <h1>hello</h1>
     </main>
   );
 }
